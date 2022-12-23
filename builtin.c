634e8ccd32dc56a446bb7b979efc3a79c3088ba7
chimbuchieke <ekelakachimbuchi@gmail.com>
2022-12-23 02:10:57 -0800

    task

--- builtin.c
+++ builtin.c
@@ -2,7 +2,8 @@
 
 /**
  * _myexit - exits the shell
- * @info: Structure containing potential arguments Used to maintain
+ * @info: Structure containing potential 
+ * arguments Used to maintain
  * constant function prototype.
  * Return: exits with a given exit status
  * (0) if info.argv[0] != "exit"
@@ -41,7 +42,8 @@ int _mycd(info_t *info)
 	s = getcwd(buffer, 1024);
 	
 	if (!s)
-		_puts("TODO: >>getcwd failure emsg here<<\n");
+		_puts("TODO: >>getcwd
+				failure emsg here<<\n");
 	if (!info->argv[1])
 	{
 		dir = _getenv(info, "HOME=");
@@ -52,7 +54,8 @@ int _mycd(info_t *info)
 		else
 			chdir_ret = chdir(dir);
 	}
-	else if (_strcmp(info->argv[1], "-") == 0)
+	else if (_strcmp
+			(info->argv[1], "-") == 0)
 	{
 		if (!_getenv(info, "OLDPWD="))
 		{
@@ -60,29 +63,38 @@ int _mycd(info_t *info)
 			_putchar('\n');
 			return (1);
 		}
-		_puts(_getenv(info, "OLDPWD=")), _putchar('\n');
-		chdir_ret = /* TODO: what should this be? */
+		_puts(_getenv(info,
+					"OLDPWD=")), _putchar('\n');
+		chdir_ret =
+			/* TODO: what should this be? */
 			chdir((dir = _getenv(info,
 							"OLDPWD=")) ? dir : "/");
 	}
 	else
-		chdir_ret = chdir(info->argv[1]);
+		chdir_ret = chdir(
+				info->argv[1]);
 	if (chdir_ret == -1)
 	{
-		print_error(info, "can't cd to ");
-		_eputs(info->argv[1]), _eputchar('\n');
+		print_error(info,
+				"can't cd to ");
+		_eputs(info->argv[1])
+			, _eputchar('\n');
 	}
 	else
 	{
-		_setenv(info, "OLDPWD", _getenv(info, "PWD="));
-		_setenv(info, "PWD", getcwd(buffer, 1024));
+		_setenv(info, "OLDPWD",
+				_getenv(info, "PWD="));
+		_setenv(info, "PWD",
+				getcwd(buffer, 1024));
 	}
 	return (0);
 }
 
 /**
- * _myhelp - changes the current directory of the process
- * @info: Structure containing potential
+ * _myhelp - changes the current 
+ * directory of the process
+ * @info: Structure containing 
+ * potential
  * arguments Used to maintain
  * constant function prototype.
  * Return: Always 0
@@ -91,10 +103,12 @@ int _myhelp(info_t *info)
 {
 	char **arg_array;
 	arg_array = info->argv;
-	_puts("help call works.
-			Function not yet implemented \n");
+	
+	_puts("help call works.Function
+			not yet implemented \n");
 	
 	if (0)
-		_puts(*arg_array); /* temp att_unused workaround */
+		_puts(*arg_array);
+	/* temp att_unused workaround */
 	return (0);
 }
